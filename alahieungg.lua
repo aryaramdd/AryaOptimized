c740f8f47b92e304b8f631f03a11e857c9386ef3
Arya R <39179343+aryaramdd@users.noreply.github.com>
2026-04-05 03:39:45 +0700

    Update alahieungg.lua

--- alahieungg.lua
+++ alahieungg.lua
@@ -103,7 +103,7 @@ task.spawn(function()
     local Settings = settings()
     pcall(function()
         Settings.Rendering.QualityLevel       = Enum.QualityLevel.Level01
-        Settings.Rendering.MeshPartDetailLevel = Enum.MeshPartDetailLevel.Disabled
+        Settings.Rendering.MeshPartDetailLevel = Enum.MeshPartDetailLevel.Level0
         Settings.Rendering.EagerBulkExecution  = true
     end)
 
